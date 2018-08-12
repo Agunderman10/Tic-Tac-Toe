2c98d9dc3ed258478ec0657faea06d9e58c32767
Andrew Gunderman <agunderman10@gmail.com>
2018-08-11 21:02:39 -0400

    -Minor additions

---
+++ WpfApp1/WpfApp1/ContentType.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApp1
+{
+    //Value a button currently contains
+    public enum ContentType
+    {
+        //Button is empty(hasn't been clicked yet)
+        Free,
+        //Button contains O
+        ContentO, 
+        //Button contains X
+        ContentX
+    }
+}
--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
@@ -20,9 +9,49 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+
+        //Holds the current results of buttons in the active game
+        private ContentType[] ContentResults;
+
+        //True if it is player 1's turn(X) or player 2's turn (O)
+        private bool Player1Turn;
+
+        //True if the game has ended
+        private bool GameEnded;
+
+        #endregion
+
+        #region Initial Constructor
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
         }
+        #endregion
+
+        #region NewGame
+        private void NewGame()
+        {
+            //Create a new blank array of free buttons
+            ContentResults = new ContentType[9];
+
+
+            //Iterates through ContentsResults and sets all contained buttons to Free
+            for (var i = 0; i < ContentResults.Length; i++)
+            {
+                ContentResults[i] = ContentType.Free;
+            }
+            //Makes sure that Player 1 starts the game
+            Player1Turn = true;
+
+            //Integrate all buttons on the UI grid
+            Container.Children.Cast<Button>().ToList().ForEach(Button => 
+            {
+                Button.Content = string.Empty;
+            });
+        }
+        #endregion
     }
 }
